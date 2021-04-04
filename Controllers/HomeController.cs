93c435d66a3880e4a7a7803c60c8b493735e52d5
dpricop <pricopdumitru94@gmail.com>
2021-04-04 21:19:05 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiDisertatie.Controllers
+{
+
+    public class HomeController : ControllerBase
+    {
+
+        public IActionResult Index()
+        {
+
+            return Ok("200 result");
+        }
+    }
+}
