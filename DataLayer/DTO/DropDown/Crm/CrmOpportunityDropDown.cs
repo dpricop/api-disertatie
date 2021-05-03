7f27cc61460172161f808b8cc32716b5dc42127e
dpricop <pricopdumitru94@gmail.com>
2021-05-03 17:57:17 +0300

    DTO CrmOpportunityDropDown - has been added

---
+++ DataLayer/DTO/DropDown/Crm/CrmOpportunityDropDown.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ApiDisertatie.DataLayer.Models
+{
+    public partial class CrmOpportunityDropDown
+    {
+        public int IdOpportunity { get; set; }
+        public string OppDescriere { get; set; }
+    }
+}
